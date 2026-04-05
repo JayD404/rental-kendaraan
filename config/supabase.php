c1a8efdd474e4cbfcc18600215d2bbee4f9a5a3f
JayD404 <jayadi.jyd018@gmail.com>
2026-04-05 19:54:47 +0800

    Add files via upload

---
+++ config/supabase.php
@@ -0,0 +1,6 @@
+<?php
+// config/supabase.php
+
+define('SUPABASE_URL', 'https://supabase.com/dashboard/project/qknthjqazdjyyyhrlpwv');
+define('SUPABASE_KEY', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFrbnRoanFhemRqeXl5aHJscHd2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MzcyNDMxNywiZXhwIjoyMDg5MzAwMzE3fQ.WqAoJzLFSH4HMas_bgkqLqyihyhxW46YiWgIChkf5Ho'); // pakai service_role key, bukan anon
+define('SUPABASE_BUCKET', 'kendaraan'); // nama bucket di Supabase Storage
